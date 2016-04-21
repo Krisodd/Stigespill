1383d96c93eedb33211a7b42d3a69679ab0fe624
Kevin294 <kevin.arnesen@hotmail.com>
2016-04-21 14:19:29 +0200

    brikke

--- src/no/hib/dat102/Spiller.java
+++ src/no/hib/dat102/Spiller.java
@@ -1,5 +1,19 @@
 package no.hib.dat102;
 
 public class Spiller {
+	
+	int plassering;
+	String brikkefarge;
+	Brikke brikke;
+	
+	public Spiller(int spillernr){
+		plassering = 0;
+		brikke = new Brikke(spillernr);
+		
+	}
+	
+	public Brikke getBrikke(){
+		return brikke;
+	}
 
 }
