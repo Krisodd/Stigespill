6fe139175a2683836a8ff6da941d765b02307b3e
krisodd <seccynet@gmail.com>
2016-04-21 15:21:13 +0200

    Bygget litt på Spill

--- src/no/hib/dat102/Spill.java
+++ src/no/hib/dat102/Spill.java
@@ -1,9 +1,19 @@
 package no.hib.dat102;
 
 public class Spill {
+	int currentTur;
+	Spiller[] spillere;
 	
-	public Spill(){
+	public Spill(Spiller[] spillere){
 		Brett brett = new Brett();
+		currentTur=0;
+		this.spillere = spillere;
+	}
+	public void flyttBrikke(Spiller spiller) {
+		
+	}
+	public int hvemSinTur(){
+		return currentTur;
 	}
 
 }
