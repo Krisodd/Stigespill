3858bc5f5e8bb3b1949ad66459c2c89563523661
Kevin294 <kevin.arnesen@hotmail.com>
2016-04-21 14:25:41 +0200

    123

--- src/no/hib/dat102/Klient.java
+++ src/no/hib/dat102/Klient.java
@@ -1,5 +1,16 @@
 package no.hib.dat102;
 
+import java.util.Scanner;
+
 public class Klient {
 
+	public void Start(){
+		System.out.println("Hei og velkommen til stigespill");
+		System.out.println("Hvor mange spillere er dere?(2-4)");
+		Scanner tast = new Scanner(System.in);
+		int antall = tast.nextInt();
+		if(antall >= 2 && antall <= 4){
+			for(int i = 0; i<=antall)
+		}
+	}
 }
