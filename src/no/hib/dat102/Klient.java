ac04b4284aeb0fc9953481b9caab2c90db96a892
Kevin294 <kevin.arnesen@hotmail.com>
2016-04-21 14:34:16 +0200

    op

--- src/no/hib/dat102/Klient.java
+++ src/no/hib/dat102/Klient.java
@@ -5,12 +5,21 @@ import java.util.Scanner;
 public class Klient {
 
 	public void Start(){
+		Spill spill = new Spill();
+		
 		System.out.println("Hei og velkommen til stigespill");
 		System.out.println("Hvor mange spillere er dere?(2-4)");
 		Scanner tast = new Scanner(System.in);
 		int antall = tast.nextInt();
 		if(antall >= 2 && antall <= 4){
-			for(int i = 0; i<=antall)
+			Spiller[] spillere = new Spiller[antall];
+			for(int i = 0; i<=antall; i++){
+				spillere[i] = new Spiller(i+1);
+			}
+		}else{
+			
 		}
+		
+		tast.close();
 	}
 }
--- src/no/hib/dat102/Spill.java
+++ src/no/hib/dat102/Spill.java
@@ -1,5 +1,9 @@
 package no.hib.dat102;
 
 public class Spill {
+	
+	public Spill(){
+		Brett brett = new Brett();
+	}
 
 }
