90a7e122404e822aa956148ad90a7692cb396493
krisodd <seccynet@gmail.com>
2016-04-21 13:15:15 +0200

    Added brett (for real)

--- src/no/hib/dat102/Brett.java
+++ src/no/hib/dat102/Brett.java
@@ -1,5 +1,17 @@
 package no.hib.dat102;
 
 public class Brett {
-
+	final int ANTALL_RUTER = 100;
+	final int ANTALL_SLANGER = 10;
+	final int ANTALL_STIGER = 10;
+	
+	Rute[] ruter = new Rute[ANTALL_RUTER];
+	Stige[] stiger = new Stige[ANTALL_STIGER];
+	Slange[] slanger = new Slange[ANTALL_SLANGER];
+	
+	public Brett() { 
+		for(int i=1;i<=ANTALL_RUTER;i++) { // populate board
+			
+		}
+	}
 }
