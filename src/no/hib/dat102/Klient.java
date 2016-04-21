49019ced3ac07d5fc6a2512734615ada88d039df
Kevin294 <kevin.arnesen@hotmail.com>
2016-04-21 15:01:49 +0200

    asd

--- src/no/hib/dat102/Klient.java
+++ src/no/hib/dat102/Klient.java
@@ -7,11 +7,14 @@ public class Klient {
 	public void Start(){
 		
 		Spill spill = new Spill();
+		
 		System.out.println("Hei og velkommen til stigespill");
 		System.out.println("Hvor mange spillere er dere?(2-4)");
+		
 		Scanner tast = new Scanner(System.in);
 		int antall = tast.nextInt();
 		Spiller[] spillere = new Spiller[antall];
+		
 		boolean valg = false;
 		while(!valg){
 			if(antall >= 2 && antall <= 4){
@@ -27,7 +30,10 @@ public class Klient {
 		}
 		System.out.println("Dere har valgt "+antall+" antall spillere");
 		
-		
+		boolean vinner = false;
+		while(!vinner){
+			
+		}
 		tast.close();
 	}
 }
