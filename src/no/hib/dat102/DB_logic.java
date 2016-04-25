7d43480b3daa4752696685a1cb42dc3aa6c7d7d2
Thomas <Thomas@158.37.228.194>
2016-04-25 12:43:59 +0200

    hei

--- src/no/hib/dat102/DB_logic.java
+++ src/no/hib/dat102/DB_logic.java
@@ -1,5 +1,25 @@
 package no.hib.dat102;
 
-public class DB_logic {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.RollbackException;
 
+public class DB_logic {
+	
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("Eclipselink_JPA");
+    EntityManager em = emf.createEntityManager();
+    
+    try {
+        em.getTransaction().begin();
+        em.persist(kunde);
+        em.getTransaction().commit();
+    } catch (RollbackException e) {
+    	System.out.println(e);
+        em.getTransaction().rollback();
+    }
+    
+    
+    
+    
 }
